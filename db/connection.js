c2cfda9a53760531e633079a4e289cbf32262810
Amrit Shukla <syeadmaazahmed@fmail.com>
2022-07-31 22:30:49 +0530

    database

--- db/connection.js
+++ db/connection.js
@@ -1,11 +1,8 @@
 const mongoose = require("mongoose");
-const DB = process.env.MONGODB_URI;
+const DB = process.env.DATABSE;
 
 mongoose
-  .connect(DB, {
-    useNewUrlParser: true,
-    useUinfiedTopology: true
-  })
+  .connect(DB)
   .then(() => {
     console.log("Connection Successful");
   })
